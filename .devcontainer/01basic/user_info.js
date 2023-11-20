a3bce73c28af460753d181b127424aeab7759711
samso1457 <151379780+samso1457@users.noreply.github.com>
2023-11-20 20:35:31 +0000

    datatype

---
+++ .devcontainer/01basic/user_info.js
@@ -0,0 +1,28 @@
+const accountId = 123 // const use to donot change
+let accountEmail = "sam@test.com" /* let and var both are use to variable 
+                                   {} name is scope not use var ole use var now use only let 
+var me for loop ya koi or use {} karne pe problem karta hai is liye ab var ke jajha lat use hota hai 
+                                   */
+var accountPassword = "1234"
+accountCity = "Varanasi"
+
+console.log(accountCity);   //log type ten pass tab to shortcut
+console.table([accountId,accountEmail,accountPassword])
+
+//node .devcontainer/01basic/user_info.js 
+//alert ( 3 + 3) we are using nodejs , not browser 
+ // "use strict" // treat all js code as newer version 
+
+console.log(3+3) // its work to use arthmatic operation 
+
+/* number => 2 power of 53
+bigint == long number
+string = ""
+boolean => true/false
+null => standalone value  // null is like empety
+undefined =>  its define  undefined
+symbol => like unique ness
+object  // null return oblect type 
+console.log(typeof "sam"); age // to find the type of value 
+
+*/
